1ac6be6197f8b0633910c9af718963b427e912c1
Mt01mottiy <motitai29@icloud.com>
2019-08-15 14:59:32 +0000

    upload

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,5 @@ class User < ApplicationRecord
   validates :password,presence: true,uniqueness: true,length: { in: 8..32 },format:/\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{8,100}+\z/i
   
   has_secure_password
+  has_many :topics
 end
